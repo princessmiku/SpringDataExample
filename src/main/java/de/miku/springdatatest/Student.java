6fe3262950e603318615c9b5c9ee17b948fc2159
Miku <mia.nakano@icloud.com>
2022-02-13 21:59:17 +0100

    Initial commit

---
+++ src/main/java/de/miku/springdatatest/Student.java
@@ -0,0 +1,95 @@
+package de.miku.springdatatest;
+
+
+import javax.persistence.*;
+
+@Table(name = "student", uniqueConstraints = {
+        @UniqueConstraint(name = "student_email_unique", columnNames = "email")
+})
+@Entity(name = "Student") // name of table
+public class Student {
+
+    @Id
+    @SequenceGenerator(
+            name = "student_sequence",
+            sequenceName = "student_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "student_sequence"
+    )
+    @Column(name = "id", updatable = false) // set specific column settings
+    private Long id;
+    @Column(name = "first_name", nullable = false, columnDefinition = "TEXT")
+    private String firstName;
+    @Column(name = "last_name", nullable = false, columnDefinition = "TEXT")
+    private String lastName;
+    @Column(name = "email", nullable = false, columnDefinition = "TEXT")
+    private String email;
+    @Column(name = "age", nullable = false)
+    private int age;
+
+    public Student() {
+
+    }
+
+    public Student(Long id, String firstName, String lastName, String email, int age) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.age = age;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
